111bcbd0fb51811c1ed536ae69a87ac5e972d8c3
Vincius Thiago <vinicius.thiago@azulseguros.com.br>
2024-06-26 15:57:31 -0300

    Inclusao de novos scripts para execução dos testes.

--- src/test/resources/db/migration/V3__insere_pagamentos.sql
+++ src/test/resources/db/migration/V3__insere_pagamentos.sql
@@ -1,2 +1,4 @@
 INSERT INTO pagamentos (valor,nome,numero,expiracao,codigo,status,forma_de_pagamento_id,pedido_id) VALUES
 	 (900.00,'Ricardo','12345678','10/29','123','CONFIRMADO',1,1);
+INSERT INTO pagamentos (valor,nome,numero,expiracao,codigo,status,forma_de_pagamento_id,pedido_id) VALUES
+	 (10.00,'Thiago','12345678','10/29','123','CONFIRMADO',1,1);
